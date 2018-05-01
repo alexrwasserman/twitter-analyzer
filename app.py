dfe7f9356e91a362df00567026892d3deeffd8d8
Alex Wasserman <alexrwasserman@gmail.com>
2018-05-01 13:42:31 -0700

    base app

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template
+import controllers
 
 # Initialize Flask app with the template folder address
 app = Flask(__name__, template_folder='templates')
+
+app.register_blueprint(controllers.main)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
